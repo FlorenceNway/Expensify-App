4d454126d918a26e5225bbc0409276660673f4ed
Florence Nway <florence@Florences-iMac.local>
2019-10-04 11:15:38 +0100

    router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditPage from '../components/EditPage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
+
+
+//create new functional component of browser router
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header/>
+                <Switch>
+                    <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                    <Route path="/create" component={AddExpensePage} />
+                    <Route path="/edit" component={EditPage} />
+                    <Route path="/help" component={HelpPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+        </div>
+    </BrowserRouter>
+)
+
+export default AppRouter;
