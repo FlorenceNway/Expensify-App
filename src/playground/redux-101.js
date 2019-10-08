381202132349b923fe253b63dff906b45745d766
Florence Nway <florence@Florences-iMac.local>
2019-10-08 16:41:13 +0100

    added more comments

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,6 +1,6 @@
 import {createStore} from 'redux'; //wants to create a store
 
-//Action generators - function that return action objects
+//ACTION GENERATORS - function that return action objects
 
 const incrementCount = ({incrementBy = 1} = {}) => ({ 
     type: 'INCREMENT',
@@ -14,8 +14,11 @@ const decrementCount = ({decrementBy = 1} = {}) => ({
     decrementBy
 })
 
-//make a store
-const store = createStore((state = {count: 0},action) => {
+//Reducer - what to do base on actions
+//1. Reducers are pure functions ,they are not interacting with ousite variables(output is determined by the input)
+//2. never change state or action
+
+const countReducer = (state = {count: 0},action) => {
     switch (action.type) {
         case 'INCREMENT':
             //const incrementBy = typeof action.incrementBy === 'number'? action.incrementBy:1;
@@ -38,15 +41,17 @@ const store = createStore((state = {count: 0},action) => {
         default:
             return state    
     }
-    
-})
+}
+
+//make a store
+const store = createStore(countReducer());
 //state= currentstate (use default state provided if no state change)
 store.getState(); //return current state obj
 
 
 console.log('initial value',store.getState()); 
 
-//fire every single time the store changes
+//fire every single time the store changes(to track the changes)
 store.subscribe(() => {
     console.log(store.getState());
 });
@@ -74,6 +79,10 @@ store.dispatch({
     type: 'RESET'
 })
 
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
 // store.dispatch({
 //     type: 'DECREMENT'
 // })
@@ -81,9 +90,6 @@ store.dispatch({
 //     type: 'INCREMENT'
 // })
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+
 
 console.log('final result',store.getState()); 
