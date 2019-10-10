04865f51fa2b91c0286dbcf30c9a2a77967d0370
Florence Nway <florence@Florences-iMac.local>
2019-10-10 15:52:16 +0100

    work with provider

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
 //install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'; //npm add react-redux@5.0.5 (API)
+//provider let individula components to access the store without manually passing around
 import AppRouter from './routers/AppRouter'
 //BR - once to create new router, R-route for every single page
-import 'normalize.css/normalize.css'
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 // Router - provide path we want to match for and what we want to do when the user visit that path
 
-ReactDOM.render(<AppRouter />,document.getElementById('app'))
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+
+
+ReactDOM.render(jsx, document.getElementById('app'))
 
 
