44c13ff0b18b9ee61f2ead62f78e6290554b7414
Florence Nway <florence@Florences-iMac.local>
2019-10-10 15:50:10 +0100

    all action,reducer,in one(exercise)

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -33,6 +33,35 @@ const removeExpense = ({id} = {}) => ({ //destructure the object , if no, empty 
     id
 });
 
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+})
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
+
+const sortByAmount = (amount = '') => ({
+    type: 'SORT_BY_AMOUNT',
+    amount
+})
+const sortByDate = (date = '') => ({
+    type: 'SORT_BY_DATE',
+    date
+})
+
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+})
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+})
+
 //WE GONNA CREATE 2 REDUCERS TO HANDLE THESE TWO OBJECTS(demoState):
 //REDUCERS:
     //Expenses Reducer
@@ -44,6 +73,17 @@ const expensesReducer = (state = expenseReducerDefaultState, action) => {
             //return state.concat(action.expense);
         case 'REMOVE_EXPENSE':
             return state.filter(({id})=>  id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense)=>{
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates //want to overwrite the exsiting ones with updates(see below examples)
+                    }
+                } else {
+                    return expense;
+                }
+            })    
         default:
             return state;
     }
@@ -59,12 +99,56 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
-        case 'ADD_EXPENSE':
-            return filtersReducerDefaultState;
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            } 
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            }  
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            }
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            }            
         default:
             return state;
     }
 };
+
+//Get visible expenses
+const getVisibleExpenses = (expenses,{text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense)=> {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        //figure out if expenses.description as the text variable string inside of it
+
+        return startDateMatch && endDateMatch && textMatch; //return true or false
+    }).sort((a,b) => {
+        if(sortBy == 'date') {
+            return a.createdAt < b.createdAt ? 1: -1
+        }else if(sortBy == 'amount') {
+            return a.amount < b.amount ? 1: -1 //1 mean come first in the list
+        }
+    });
+
+}
+
 
 //Store Creation:
 const store = createStore(
@@ -74,16 +158,31 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+//console.log(store.getState());
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}))
-const expenseTwo = store.dispatch(addExpense({description: 'Electricity', amount: 50}))
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: -1000}))
+const expenseTwo = store.dispatch(addExpense({description: 'Electricity', amount: 50, createdAt: -1500}))
 
-store.dispatch(removeExpense({id: expenseOne.expense.id}));
+// store.dispatch(removeExpense({id: expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter('e'));
+
+store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+//store.dispatch(setStartDate(-125));
+// store.dispatch(setStartDate());
+
+//store.dispatch(setEndDate(1250));
+
 
 //WE GONNA CREATE 2 REDUCERS TO HANDLE THESE TWO OBJECTS:
 const demoState = {
@@ -109,8 +208,14 @@ const user ={
     name: 'Jen',
     age: 24
 };
-console.log({
-    ...user,
-    location:'London',
-    age: 27
-})
+// console.log({
+//     ...user,
+//     location:'London',
+//     age: 27 //overwrite
+// })
+
+//Timestamps ( calculated by miliseconds)
+//always start at January 1st 1970 (it is called unix epoch)
+//it is valid to have - value, eg, 33400 (33.4 second after January 1st 1970)
+//, 10, -203 (- is less than 1970, might be 1969)
+
